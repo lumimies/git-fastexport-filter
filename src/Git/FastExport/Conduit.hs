284bc58db8b363555f4258b5b9caa48254d6ca07
Zohar Kelrich <zohar@rayv.com>
2012-08-29 18:01:09 +0300

    Add fancy conduit combinator for branch splitting
    I'm not sure if I like it: It's slightly more general than it needs to be
    (having only a subset that's allowed to stream immediately), and seems
    to be doing three things. But I'm not sure how I would disentangle it.
    
    Also, it's a bit long and unclear, and has a lot of lifts in it. I wonder
    if it can be made more beautiful.

--- src/Git/FastExport/Conduit.hs
+++ src/Git/FastExport/Conduit.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoMonomorphismRestriction, ImpredicativeTypes, OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Git.FastExport.Conduit where
 
 import Data.Conduit
@@ -7,7 +8,8 @@ import qualified Data.Conduit.Attoparsec as CA
 import qualified Data.Conduit.Blaze as CB
 import qualified Blaze.ByteString.Builder as BZ
 import qualified Git.FastExport.Parser as P
-import Data.ByteString
+import qualified Data.Map as M
+import Data.ByteString (ByteString)
 import Control.Monad.Base
 import Control.Applicative
 import Data.Attoparsec as A
@@ -16,6 +18,7 @@ import Git.FastExport.Types
 import Data.Void
 import Data.Monoid((<>))
 import Data.Maybe
+import Control.Monad (when, unless)
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Class
 import Git.FastExport.AuthorFilter
@@ -68,9 +71,55 @@ data SplitterState = SS
 	, ssExtraBranches :: [(Branch,[GitEvent])]
 	, ssCommitHeader  :: Maybe CommitHeader
 	}
-{-	
-splitByBranches :: Monad m => (Path -> (Path,Branch)) -> GInfConduit GitEvent m GitEvent
-splitByBranches clsfy = transPipe (flip evalState (SS Nothing [] Nothing)) $ awaitForever
-	$ \i -> case i of
-		GECommitHeader ch -> lift . modify $ \s -> s{ssCommitHeader = ch}
-		GEChange c        -> -}
+data StuffSplitterState k v = SSS
+	{ sssCurrentBucket :: Maybe k
+	, sssOtherBuckets :: M.Map k v
+	}
+
+-- | Map one input into many sets of outputs, grouped by key, and then emit sets
+-- by key order. Allows immediately streaming the first of some subset of keys
+-- instead of collecting it.
+splitStuff :: (Monad m, Ord k) => 
+	(a -> [(k,GSource m b)]) -- ^ Map input to sources for certain buckets
+	-> (a -> m Bool)         -- ^ Whether to flush all buckets after this
+	-> (k -> m Bool)         -- ^ Whether this bucket may be streamed through
+	-> GInfConduit a m b
+splitStuff classify flushAfterP allowStreamThroughP = transPipe (flip evalStateT (SSS Nothing M.empty)) $ awaitForever
+	$ \i -> do
+		let kvs = classify i
+		unless (null kvs) $ do
+			lift . modify
+				$ \s -> s{sssCurrentBucket = Just $ fromMaybe (fst . head $ kvs) $ sssCurrentBucket s }
+			Just curr <- lift . gets $ sssCurrentBucket
+			mapM_ yieldStuff kvs
+		flush <- lift . lift $ flushAfterP i
+		when flush $ do
+			srcs <- lift . gets $ M.elems . sssOtherBuckets
+			lift . put $ SSS Nothing M.empty
+			transPipe lift $ sequence_ srcs
+	where
+		yieldStuff (k,src :: GSource m b) = do
+			haveCurrent <- lift . gets $ isJust . sssCurrentBucket
+			unless haveCurrent $ do
+				allowFlush <- lift . lift $ allowStreamThroughP k
+				when allowFlush $
+					lift . modify $ \s ->s{sssCurrentBucket = Just k}
+			curr <- lift . gets $ sssCurrentBucket
+			case curr of
+				Just k' | k == k' -> 
+					transPipe lift src
+				Nothing           -> 
+					lift . modify $ \s -> s{sssOtherBuckets = M.insertWith (>>) k src (sssOtherBuckets s)}				
+
+{-splitByBranches :: (MonadThrow m) => (GChange () -> [(Branch,[GitEvent])]) -> GInfConduit GitEvent m GitEvent
+splitByBranches clsfy = transPipe (flip evalStateT Nothing) $
+		splitStuff classifyEvent isCommitEnded
+	where
+		classifyEvent (GECommitHeader ch) = lift . put $ Just (ch,[])
+		classifyEvent (GEChange c)        = do
+			let bs = clsfy c
+			s <- get
+			case s of
+				Nothing -> 
+			(Just (ch,bs'), emitEvents )
+-}
